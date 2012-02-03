2863d853b6187117c45e0deb68b7d7abd455dd26
Andrew Hutchings <andrew@linuxjedi.co.uk>
2012-02-03 14:40:08 +0000

    Update for puppet installation
    Fixes this up to be used with puppet.  DO NOT MERGE UNTIL SWITCHING TO PUPPET!
    
    Change-Id: I845726a6496660da90e3af03c5caf559f24e53a7

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import ConfigParser
 cp = ConfigParser.ConfigParser()
 cp.read("planet.ini")
 
-if cp.get('Planet', 'output_dir') != '/srv/planet':
+if cp.get('Planet', 'output_dir') != '/srv/planet/openstack':
   sys.exit(1)
 
